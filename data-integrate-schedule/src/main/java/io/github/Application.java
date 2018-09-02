4f3da3a2d583366b554344bdeab8f8fc68e5f0ea
li_1987245 <li_1987245@163.com>
2018-09-03 06:50:09 +0800

    init

--- data-integrate-schedule/src/main/java/io/github/Application.java
+++ data-integrate-schedule/src/main/java/io/github/Application.java
@@ -1,8 +1,49 @@
 package io.github;
 
+import io.github.entity.SimpleJob;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
 /**
  * @Author: jinwei.li@100credit.com
  * @Date: 2018/8/30 15:56
  */
+@Slf4j
 public class Application {
+
+    public static void main(String[] args) {
+        try {
+            // Grab the Scheduler instance from the Factory
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+            // and start it off
+            scheduler.start();
+            // define the job and tie it to our HelloJob class
+            JobDetail job = newJob(SimpleJob.class)
+                    .withIdentity("job1", "group1")
+                    .build();
+
+            // Trigger the job to run now, and then repeat every 40 seconds
+            Trigger trigger = newTrigger()
+                    .withIdentity("trigger1", "group1")
+                    .startNow()
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(40)
+                            .repeatForever())
+                    .build();
+
+            // Tell quartz to schedule the job using our trigger
+            scheduler.scheduleJob(job, trigger);
+
+//            scheduler.shutdown();
+
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
 }
