1b214cdc00ee7b92bfb82b4ddd0b8ae0dd2a34fa
li_1987245 <li_1987245@163.com>
2019-03-10 22:10:12 +0800

    merge

---
+++ data-integrate-parser/src/main/java/io/github/mysql/visitor/MysqlVisitor.java
@@ -0,0 +1,159 @@
+package io.github.mysql.visitor;
+
+import io.github.mysql.MySqlParser;
+import io.github.mysql.MySqlParserBaseVisitor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MysqlVisitor extends MySqlParserBaseVisitor {
+
+
+    @Override
+    public Object visitQuerySpecification(MySqlParser.QuerySpecificationContext ctx) {
+        String select = ctx.selectElements().getText();
+        log.info("select:{}", select);
+        ctx.selectSpec().forEach(s->{
+            log.info("spec:{}",s.getText());
+        });
+        String from = ctx.fromClause().getText();
+        log.info("from:{}", from);
+        return super.visitQuerySpecification(ctx);
+    }
+
+    /**
+     * 5
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitTableSources(MySqlParser.TableSourcesContext ctx) {
+        ctx.tableSource().forEach(t->{
+            log.info("table:{}",t.getText());
+        });
+        return super.visitTableSources(ctx);
+    }
+
+    @Override
+    public Object visitTableSourceBase(MySqlParser.TableSourceBaseContext ctx) {
+        ctx.joinPart().forEach(j->{
+            log.info("join:{}",j.getText());
+            j.children.forEach(p->{
+                log.info("part:{}",p.getText());
+            });
+        });
+        return super.visitTableSourceBase(ctx);
+    }
+
+    @Override
+    public Object visitTableSourcesItem(MySqlParser.TableSourcesItemContext ctx) {
+        return super.visitTableSourcesItem(ctx);
+    }
+
+    @Override
+    public Object visitInnerJoin(MySqlParser.InnerJoinContext ctx) {
+        return super.visitInnerJoin(ctx);
+    }
+
+    @Override
+    public Object visitOuterJoin(MySqlParser.OuterJoinContext ctx) {
+
+        System.out.println(ctx.tableSourceItem().getText()+"...."+ctx.expression().getText());
+        return super.visitOuterJoin(ctx);
+    }
+
+    /**
+     * 7
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitOrderByClause(MySqlParser.OrderByClauseContext ctx) {
+        return super.visitOrderByClause(ctx);
+    }
+
+    /**
+     * 8
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitOrderByExpression(MySqlParser.OrderByExpressionContext ctx) {
+        return super.visitOrderByExpression(ctx);
+    }
+
+    @Override
+    public Object visitSubqueryTableItem(MySqlParser.SubqueryTableItemContext ctx) {
+        return super.visitSubqueryTableItem(ctx);
+    }
+
+
+    @Override
+    public Object visitQueryExpression(MySqlParser.QueryExpressionContext ctx) {
+        return super.visitQueryExpression(ctx);
+    }
+
+    /**
+     * 1
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitSelectSpec(MySqlParser.SelectSpecContext ctx) {
+        return super.visitSelectSpec(ctx);
+    }
+
+    /**
+     * 2
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitSelectElements(MySqlParser.SelectElementsContext ctx) {
+        return super.visitSelectElements(ctx);
+    }
+
+    /**
+     * 3
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitSelectColumnElement(MySqlParser.SelectColumnElementContext ctx) {
+        return super.visitSelectColumnElement(ctx);
+    }
+
+    @Override
+    public Object visitSelectExpressionElement(MySqlParser.SelectExpressionElementContext ctx) {
+        return super.visitSelectExpressionElement(ctx);
+    }
+
+    /**
+     * 4
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitFromClause(MySqlParser.FromClauseContext ctx) {
+        return super.visitFromClause(ctx);
+    }
+
+    /**
+     * 6
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitGroupByItem(MySqlParser.GroupByItemContext ctx) {
+        return super.visitGroupByItem(ctx);
+    }
+
+    /**
+     * 9
+     * @param ctx
+     * @return
+     */
+    @Override
+    public Object visitLimitClause(MySqlParser.LimitClauseContext ctx) {
+        return super.visitLimitClause(ctx);
+    }
+}
--- data-integrate-parser/src/test/java/BasicTest.java
+++ data-integrate-parser/src/test/java/BasicTest.java
@@ -1,9 +1,16 @@
-import io.github.CalcLexer;
-import io.github.CalcParser;
+import io.github.calc.CalcLexer;
+import io.github.calc.CalcParser;
+import io.github.calc.listener.Evaluator;
+import io.github.calc.visitor.EvalVisitor;
+import io.github.mysql.MySqlLexer;
+import io.github.mysql.MySqlParser;
+import io.github.mysql.utils.UpperCaseCharStream;
+import io.github.mysql.visitor.MysqlVisitor;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
 
 /**
@@ -24,14 +31,30 @@ public class BasicTest {
         System.out.println(tree.toStringTree(parser));
         System.out.println();
 
-//        System.out.println("Visitor:");
-//        EvalVisitor evalByVisitor = new EvalVisitor();
-//        evalByVisitor.visit(tree);
-//        System.out.println();
-//
-//        System.out.println("Listener:");
-//        ParseTreeWalker walker = new ParseTreeWalker();
-//        Evaluator evalByListener = new Evaluator();
-//        walker.walk(evalByListener, tree);
+        System.out.println("Visitor:");
+        EvalVisitor evalByVisitor = new EvalVisitor();
+        Double d = evalByVisitor.visit(tree);
+        System.out.println(d);
+
+        System.out.println("Listener:");
+        ParseTreeWalker walker = new ParseTreeWalker();
+        Evaluator evalByListener = new Evaluator();
+        walker.walk(evalByListener, tree);
+    }
+
+    @Test
+    public void testMysql() throws Exception {
+        CharStream charStreams = new UpperCaseCharStream(CharStreams.fromString("select distinct t1.comp_name compName,t.api_code apiCode,sum(t.payment) payments from dms_payment t left join $.dms_company t1 on t.id=t1.t_id where t1.comp_name='测试' group by t1.comp_name,t.api_code order by t.api_code limit 10"));
+        MySqlLexer lexer = new MySqlLexer(charStreams);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MySqlParser parser = new MySqlParser(tokens);
+        MySqlParser.RootContext tree = parser.root();
+
+        System.out.println("LISP:");
+        System.out.println(tree.toStringTree(parser));
+        System.out.println();
+        System.out.println("Visitor:");
+        MysqlVisitor visitor = new MysqlVisitor();
+        visitor.visit(tree);
     }
 }
