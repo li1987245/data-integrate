d705fdf1ae1fdabd0ff7e04f0308ec9a77784389
li_1987245 <li_1987245@163.com>
2019-04-22 12:51:43 +0800

    Merge

--- data-integrate-core/src/main/kotlin/io/github/Hello.kt
+++
@@ -1,6 +0,0 @@
-package io.github
-
-fun main(args: Array<String>) {
-    println("Hello, World")
-}
-
--- data-integrate-web/src/main/java/io/github/Request.java
+++
@@ -1,9 +0,0 @@
-package io.github;
-
-/**
- * @description
- * @author: jinwei.li@100credit.com
- * @date: 2018/11/9 19:38
- */
-public class Request {
-}
